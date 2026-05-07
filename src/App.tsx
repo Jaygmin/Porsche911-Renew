0858de0de94e61d85ef3b081183c4d052c96e466
Jay Min <jay@Jays-MBP.lan>
2026-05-07 17:26:57 -0400

    feat(style): Fix Page Responsive UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,30 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col">
-      <Header />
-      <div className="flex-1 bg-[url(/src/assets/Background.png)] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center sm:block">
-        <div className="flex flex-col items-center sm:items-start space-y-2 sm:ml-15 sm:mt-30 text-center sm:text-left">
-          <h1 className="md:text-5xl text-4xl sm:text-6xl font-semibold text-white tracking-tight">
-            Driven to Perfection.
-          </h1>
-          <h3 className="md:text-3xl text-2xl sm:text-4xl font-light text-white">
-            Speed, Precision, Legacy
-          </h3>
-        </div>
-        <div className="mt-8 sm:mt-0 sm:bottom-10 flex items-center justify-center sm:absolute left-1/2 right-1/2">
-          <Button
-            className="bg-transparent text-white rounded-none border-2 py-2.5 px-8 font-normal tracking-tight cursor-pointer"
-            variant={"outline"}
-          >
-            Read more
-          </Button>
+    <>
+      <div className="flex h-dvh w-full flex-col">
+        <Header />
+        <div className="flex-1 bg-[url(/src/assets/Background.png)] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center sm:block">
+          <div className="flex flex-col items-center sm:items-start space-y-2 sm:ml-15 sm:mt-30 text-center sm:text-left">
+            <h1 className="md:text-5xl text-4xl sm:text-6xl font-semibold text-white tracking-tight">
+              Driven to Perfection.
+            </h1>
+            <h3 className="md:text-3xl text-2xl sm:text-4xl font-light text-white">
+              Speed, Precision, Legacy
+            </h3>
+          </div>
+          <div className="mt-8 sm:mt-0 sm:bottom-10 flex items-center justify-center sm:absolute left-1/2 right-1/2">
+            <Button
+              className="bg-transparent text-white rounded-none border-2 py-2.5 px-8 font-normal tracking-tight cursor-pointer"
+              variant={"outline"}
+            >
+              Read more
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="w-full min-h-130 h-full"></div>
+    </>
   );
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,28 +7,26 @@ export default function Header() {
   const [lang, setLang] = useState<Lang>("EN");
 
   return (
-    <div className="w-full min-h-20 h-full px-10 py-5">
-      <header className="flex items-center justify-center">
-        <div className="hidden sm:block sm:flex-1" />
-        <img src={Logo} alt="Porsche911 Renew Page Logo" className="w-auto" />
-        <div className="sm:flex hidden flex-1 items-center justify-end gap-1">
-          <button
-            onClick={() => setLang("KO")}
-            className="text-sm font-medium transition-colors cursor-pointer"
-            style={{ color: lang === "KO" ? "#0c0c0c" : "#B6B6B6" }}
-          >
-            KO
-          </button>
-          /
-          <button
-            onClick={() => setLang("EN")}
-            className="text-sm font-medium transition-colors cursor-pointer"
-            style={{ color: lang === "EN" ? "#0c0c0c" : "#B6B6B6" }}
-          >
-            EN
-          </button>
-        </div>
-      </header>
-    </div>
+    <header className="flex min-h-20 w-full items-center justify-center px-10 py-5">
+      <div className="hidden sm:block sm:flex-1" />
+      <img src={Logo} alt="Porsche911 Renew Page Logo" className="w-auto" />
+      <div className="hidden flex-1 items-center justify-end gap-1 sm:flex">
+        <button
+          onClick={() => setLang("KO")}
+          className="cursor-pointer text-sm font-medium transition-colors"
+          style={{ color: lang === "KO" ? "#0c0c0c" : "#B6B6B6" }}
+        >
+          KO
+        </button>
+        /
+        <button
+          onClick={() => setLang("EN")}
+          className="cursor-pointer text-sm font-medium transition-colors"
+          style={{ color: lang === "EN" ? "#0c0c0c" : "#B6B6B6" }}
+        >
+          EN
+        </button>
+      </div>
+    </header>
   );
 }
