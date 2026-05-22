62367fcd7bb4dbcfcbbe385dbab7cefb43bdcd50
Jay Min <jay@Jays-MBP.lan>
2026-05-22 05:00:47 -0400

    feat(Optimization): Compress Images with WebP and Improves LCP & FCP

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    imagetools({
+      defaultDirectives: () => new URLSearchParams({ format: "webp", quality: "82" }),
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
